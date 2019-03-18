eb350ddbad6ede318244a7a4420b9a4b029485db
Morsmetus <morsmetus@gmail.com>
2019-03-18 21:10:13 +0400

    added AvenirNext font, some refactoring done, defects fixes,more refactoring TODO because of new font

--- src/components/UI/Logo/Logo.js
+++ src/components/UI/Logo/Logo.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 import ROUTES from "../../../routes/routes";
 
 const logo = props => (
-  <div className="Logo">
+  <div className={`Logo ${props.type === "white" ? "logo-white" : "logo-default"}`}>
     <NavLink to={ROUTES.HOME}>
       <span className="FirstPart">open</span>
       <span className="SecondPart">{props.projectName}</span>
