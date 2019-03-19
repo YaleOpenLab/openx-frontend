e81fc6ef5019c254ff94c767bcc186e23f675ad1
Samuel Visscher <samuel@visscher.io>
2019-03-19 14:44:51 +0400

    Integrate projectAll in redux

--- src/store/epics/index.js
+++ src/store/epics/index.js
@@ -3,34 +3,17 @@ import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/catch';
-import { ajax } from 'rxjs/observable/dom/ajax';
 import { combineEpics } from 'redux-observable';
+import { Http } from '../../services/Http';
 
-
-import {
-    FETCH_PROJECTS,
-    fetchProjectsFailure,
-    fetchProjectsSuccess
-} from "../actions";
-
-const url = 'https://evening-citadel-85778.herokuapp.com/whiskey/';
+import { FETCH_PROJECTS, fetchProjectsFailure, fetchProjectsSuccess } from '../actions';
 
 const fetchProjectsEpic = action$ => {
-    return action$
-        .ofType(FETCH_PROJECTS)
-        .switchMap(() => {
-            return ajax
-                .getJSON(url)
-                .map(data => data.results)
-                .map(whiskies => whiskies.map(whisky => ({
-                    id: whisky.id,
-                    title: whisky.title,
-                    imageUrl: whisky.img_url
-                })))
-                .map(whiskies => whiskies.filter(whisky => !!whisky.imageUrl))
-        })
-        .map(whiskies => fetchProjectsSuccess(whiskies))
-        .catch(error => Observable.of(fetchProjectsFailure(error.message)))
+  return action$
+    .ofType(FETCH_PROJECTS)
+    .switchMap(() => Http.projectAll())
+    .map(projects => fetchProjectsSuccess(projects))
+    .catch(error => Observable.of(fetchProjectsFailure(error.message)))
 };
 
 export const rootEpic = combineEpics(fetchProjectsEpic);
