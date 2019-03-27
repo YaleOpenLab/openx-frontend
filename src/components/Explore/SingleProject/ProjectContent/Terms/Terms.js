fad544a488b8cb1ab85e9a67f3052aab32ab8137
Morsmetus <morsmetus@gmail.com>
2019-03-27 19:43:17 +0400

    added unsubscribe on explore page, integrated some project detail data with API

--- src/components/Explore/SingleProject/ProjectContent/Terms/Terms.js
+++ src/components/Explore/SingleProject/ProjectContent/Terms/Terms.js
@@ -14,20 +14,20 @@ const Terms = props => (
         <div className="row">
           <div className="col-12 title no-padding border-bottom">Terms</div>
           <div className="col-12 text-description">
-            <div className="sub-title">{props.data.title}</div>
+            <div className="sub-title">{props.tempData.title}</div>
             <div className="sub-desc">
-              {props.data.description}
+              {props.tempData.description}
             </div>
           </div>
           <div className="col-12 project-table no-padding">
             <TermsTable
-              data={props.data.table}
+              data={props.data.Terms}
             />
           </div>
           <div className="col-12 security-note">
             <div className="security-title">security note</div>
             <div className="security-text">
-              {props.data.securityNote}
+              {props.tempData.securityNote}
               <span className="custom-link-to"> Link ></span>
             </div>
           </div>
