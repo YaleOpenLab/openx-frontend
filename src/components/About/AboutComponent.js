8cdff23499eb194c88b0921e221d043cf2aabff2
Morsmetus <morsmetus@gmail.com>
2019-03-29 18:55:12 +0400

    finish up about page screens

---
+++ src/components/About/AboutComponent.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./AboutComponent.scss";
+
+const AboutComponent = props => (
+  <div className="container">
+    <div className="about-container">
+      <div className="about-notice">
+        <div className="about-notice-text">
+          this section is under development
+        </div>
+      </div>
+      <div className="about-content">
+        <div className="about-content-notice">ABOUT THE PROJECT</div>
+        <div className="about-content-title">
+          Open & Frictionless <br />
+          Project Finance for a Low Carbon Planet
+        </div>
+        <div className="about-content-text">
+          <p>
+            We are building an open source platform that leverages
+            decentralization technology for disintermediation of financial
+            processes and contractual automation to drive community-owned solar
+            projects.
+          </p>
+          <p>
+            OpenSolar’s core technology seeks to streamline the flow of
+            crowd-based funds (eg. bonds and equity) to finance the deployment
+            of solar assets, allowing end-users to own these resources in a
+            short time frame through regular utility-like payments driven by
+            energy generation and consumption IoT data.
+          </p>
+        </div>
+        <div className="about-fixed-width">
+          <button className="solar-form-button solar-btn-normal">
+            go to openx wiki
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default AboutComponent;
