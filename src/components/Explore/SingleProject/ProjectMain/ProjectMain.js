62dba3ad147e774d36a6ef0d9545a053b6e034b7
Samuel Visscher <samuel@visscher.io>
2019-03-30 18:59:52 +0400

    Adjust stats

--- src/components/Explore/SingleProject/ProjectMain/ProjectMain.js
+++ src/components/Explore/SingleProject/ProjectMain/ProjectMain.js
@@ -1,28 +1,19 @@
-import React from "react";
-import ProgressBar from "../../../General/ProgressBar/ProgressBar";
-import "./ProjectMain.scss";
-import DescriptionGrid from "../../../General/DescriptionGrid/DescriptionGrid";
-import percentage from "../../../../helpers/functions/percentage";
+import React from 'react';
+import ProgressBar from '../../../General/ProgressBar/ProgressBar';
+import './ProjectMain.scss';
+import DescriptionGrid from '../../../General/DescriptionGrid/DescriptionGrid';
+import percentage from '../../../../helpers/functions/percentage';
+import { STAGES } from '../../../../helpers/enums/stages';
 
 const ProjectMain = props => {
   const data = props.data;
-  const tempData = props.tempData;
-  let descriptionItems = null;
-
-  if (data.ExplorePageSummary) {
-    let items = [];
-    Object.keys(data.ExplorePageSummary).map(key => {
-      return items.push({ value: data.ExplorePageSummary[key], desc: key });
-    });
-    descriptionItems = <DescriptionGrid list={items} />;
-  }
 
   return (
     <div className="container project-main">
       <div className="row">
         <div className="col-sm-12 col-md-7 order-md-1 order-sm-1 no-padding">
           <div className="project-main-image project-main-display-image">
-            <img src={data.DPIntroImage} alt="Project" />
+            <img src={data.DPIntroImage} alt="Project"/>
           </div>
         </div>
 
@@ -31,13 +22,13 @@ const ProjectMain = props => {
             <div className="container">
               <div className="row mx-auto">
                 <div className="col-3 mx-auto no-padding">
-                  <img src={data.DPIntroImage} alt="Project" />
+                  <img src={data.DPIntroImage} alt="Project"/>
                 </div>
                 <div className="col-3 mx-auto no-padding">
-                  <img src={data.DPIntroImage} alt="Project" />
+                  <img src={data.DPIntroImage} alt="Project"/>
                 </div>
                 <div className="col-3 mx-auto no-padding">
-                  <img src={data.DPIntroImage} alt="Project" />
+                  <img src={data.DPIntroImage} alt="Project"/>
                 </div>
               </div>
             </div>
@@ -45,53 +36,77 @@ const ProjectMain = props => {
         </div>
         <div className="col-sm-12 col-md-5 order-md-2 order-sm-3  no-padding">
           <button className="watch-button">
-            <div className="watch-icon" />
+            <div className="watch-icon"/>
           </button>
 
           <div className="projects-description-box container">
-            <div className="row">
-              <div className="desc-status col-12">
-                STAGE {data.Stage} | {tempData.status}
-              </div>
-              <div className="desc-title col-12">{data.Name}</div>
-              <div className="desc-location col-12">
-                <div className="projects-location-icon location-icon" />
-                {data.State}, {data.Country}
-              </div>
-              <div className="col-6 desc-category desc-category-separator desc-cat-category">
+            <div className="desc-status">
+              Stage {props.data.Stage} | {STAGES[props.data.Stage]}
+            </div>
+            <div className="desc-title">{data.Name}</div>
+            <div className="desc-location">
+              <div className="projects-location-icon location-icon"/>
+              {data.State}, {data.Country}
+            </div>
+            <div className="projects-description__spread">
+              <div className="desc-category desc-category-separator desc-cat-category">
                 {data.InvestmentType}
               </div>
-              <div className="col-6 desc-category desc-category-separator desc-gen-location">
+              <div className="desc-category desc-category-separator desc-gen-location">
                 {data.BrokerDealer}
               </div>
-              <div className="desc-description col-12">
-                {data.Metadata}
-                <div className="desc-description-buttons">
-                  <ul>
-                    <li>{data.Bullets.Bullet1}</li>
-                    <li>{data.Bullets.Bullet2}</li>
-                    <li>{data.Bullets.Bullet3}</li>
-                  </ul>
-                </div>
+            </div>
+
+            <div className="desc-description">
+              {data.Metadata}
+              <div className="desc-description-buttons">
+                <ul>
+                  <li>{data.Bullets.Bullet1}</li>
+                  <li>{data.Bullets.Bullet2}</li>
+                  <li>{data.Bullets.Bullet3}</li>
+                </ul>
               </div>
-              <div className="col-12 specifics-box">
-                <div className="container">
-                  <div className="row">
-                    <div className="col-6 spec-first-price spec-price-bigger">
-                      ${data.MoneyRaised}
-                    </div>
-                    <div className="col-6 spec-second-price spec-price-bigger">
-                      ${data.TotalValue}
-                    </div>
-                    <div className="col-12 ">
-                      <ProgressBar
-                        progress={percentage(data.MoneyRaised, data.TotalValue)}
-                      />
+            </div>
+            <div className="specifics-box">
+              <div className="container">
+                <div className="row">
+                  <div className="col-6 spec-first-price spec-price-bigger">
+                    ${data.MoneyRaised}
+                  </div>
+                  <div className="col-6 spec-second-price spec-price-bigger">
+                    ${data.TotalValue}
+                  </div>
+                  <div className="col-12 ">
+                    <ProgressBar
+                      progress={percentage(data.MoneyRaised, data.TotalValue)}
+                    />
+                  </div>
+                  <div className="col-12">
+                    <div className="projects-description__stats">
+                      <div className="-primary-color">
+                        <p>{data.ExplorePageSummary.Solar}</p>
+                        <h6>Solar</h6>
+                      </div>
+                      <div className="-dark-color">
+                        <p>{data.ExplorePageSummary.Storage}</p>
+                        <h6>Storage</h6>
+                      </div>
+                      <div>
+                        <p>{data.ExplorePageSummary.Return}</p>
+                        <h6>Return</h6>
+                      </div>
+                      <div>
+                        <p>{data.ExplorePageSummary.Rating}</p>
+                        <h6>Rating</h6>
+                      </div>
+                      <div>
+                        <p>{data.ExplorePageSummary.ETA}</p>
+                        <h6>Maturity</h6>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-              {descriptionItems}
             </div>
           </div>
         </div>
