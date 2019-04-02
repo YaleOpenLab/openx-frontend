11ed6c67d859e68e73c9f631e19a8fe76c54ceaf
Samuel Visscher <samuel@visscher.io>
2019-04-02 17:06:45 +0400

    Fix Forecast

--- src/components/Explore/SingleProject/ProjectContent/Forecast/Forecast.js
+++ src/components/Explore/SingleProject/ProjectContent/Forecast/Forecast.js
@@ -1,48 +1,46 @@
-import React from "react";
-import DevelopmentStage from "./DevelopmentStage/DevelopmentStage";
-import { NavLink } from "react-router-dom";
-import ROUTES from "../../../../../routes/routes";
-import ScrollableAnchor from "react-scrollable-anchor";
+import React from 'react';
+import DevelopmentStage from './DevelopmentStage/DevelopmentStage';
+import { NavLink } from 'react-router-dom';
+import ROUTES from '../../../../../routes/routes';
+import ScrollableAnchor from 'react-scrollable-anchor';
 import { STAGES } from '../../../../../helpers/enums/stages';
 import Title from '../../../../General/Title/Title';
 
 const Forecast = props => (
-  <ScrollableAnchor id={"stageforecast"}>
+  <ScrollableAnchor id={'stageforecast'}>
     <section className="Forecast">
-      <Title title="Project Stage & Forecast" />
+      <Title title="Project Stage & Forecast"/>
       <div className="container">
         <div className="row">
           <div className="col-12">
-            <div className="row mx-auto">
-              <div className="component-title col-12 no-padding margin-top">
-                <span>development stage</span>
-              </div>
-              <div className="col-12">
-                {
-                  props.data.extra && props.data.extra.stages && props.data.extra.stages.graph && (
-                    <DevelopmentStage data={props.data.extra.stages.graph} currentStage={props.data.Stage} />
-                  )
-                }
-              </div>
-              <div className="col-12 col-sm-7 mx-auto mg-top-15 forecast-info">
-                <div className="forecast-info-title">{STAGES[props.data.Stage]}</div>
-                <div className="forecast-info-sub-title">current stage</div>
-                {
-                  props.data.extra && props.data.extra.stages && props.data.extra.stages.graph && (
-                    <div className="forecast-info-content">
-                      {props.data.extra.stages.graph.stageDescription} Learn more>
-                    </div>
-                  )
-                }
-              </div>
-              <div className="col-12 mg-top-15 link-to-records">
-                {
-                  props.data.extra && props.data.extra.stages && props.data.extra.stages.url && (
-                    <NavLink to={props.data.extra.stages.url} className="small">FULL TIMELINE RECORDS ></NavLink>
-                  )
-                }
-              </div>
+            <div className="component-title col-12 no-padding">
+              <span>development stage</span>
             </div>
+          </div>
+          <div className="col-12">
+            {
+              props.data.extra && props.data.extra.stages && props.data.extra.stages.graph && (
+                <DevelopmentStage data={props.data.extra.stages.graph} currentStage={props.data.Stage}/>
+              )
+            }
+          </div>
+          <div className="col-12 col-sm-7 mx-auto mg-top-15 forecast-info">
+            <div className="forecast-info-title">{STAGES[props.data.Stage]}</div>
+            <div className="forecast-info-sub-title">current stage</div>
+            {
+              props.data.extra && props.data.extra.stages && props.data.extra.stages.graph && (
+                <div className="forecast-info-content">
+                  {props.data.extra.stages.graph.stageDescription} Learn more>
+                </div>
+              )
+            }
+          </div>
+          <div className="col-12 mg-top-15 link-to-records">
+            {
+              props.data.extra && props.data.extra.stages && props.data.extra.stages.url && (
+                <NavLink to={props.data.extra.stages.url} className="small">FULL TIMELINE RECORDS ></NavLink>
+              )
+            }
           </div>
         </div>
       </div>
@@ -50,10 +48,10 @@ const Forecast = props => (
         <div className="container">
           <div className="row">
             <div className="component-title col-12 margin-top">
-              <span>solar / </span>financial state & forecast
+              solar / financial state & forecast
             </div>
             <div className="col-12 col-sm-7 mx-auto mg-top-15 forecast-info">
-              <div className="lock-icon" />
+              <div className="lock-icon"/>
               <div className="restricted-info-sub-title">access restricted</div>
               <div className="forecast-info-content">
                 Access to this section is reserved for contract parties. Either
