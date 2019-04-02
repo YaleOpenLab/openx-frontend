780b89f618208b6bcefc89e13363e6bb8defef3c
Samuel Visscher <samuel@visscher.io>
2019-04-02 18:34:53 +0400

    Fix alignment single project header

--- src/components/Explore/SingleProject/ProjectMain/ProjectMain.js
+++ src/components/Explore/SingleProject/ProjectMain/ProjectMain.js
@@ -17,24 +17,6 @@ const ProjectMain = props => {
           </div>
         </div>
 
-        {/* <div className="col-sm-12 col-md-7 order-md-3 order-sm-2 no-padding">
-          <div className="project-main-image project-main-choose-image">
-            <div className="container">
-              <div className="row mx-auto">
-                <div className="col-3 mx-auto no-padding">
-                  <img src={data.DPIntroImage} alt="Project"/>
-                </div>
-                <div className="col-3 mx-auto no-padding">
-                  <img src={data.DPIntroImage} alt="Project"/>
-                </div>
-                <div className="col-3 mx-auto no-padding">
-                  <img src={data.DPIntroImage} alt="Project"/>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> */}
-        
         <div className="col-sm-12 col-md-5 order-md-2 order-sm-3  no-padding">
           <button className="watch-button">
             <div className="watch-icon"/>
@@ -69,41 +51,39 @@ const ProjectMain = props => {
               </div>
             </div>
             <div className="specifics-box">
-              <div className="container">
-                <div className="row">
-                  <div className="col-6 spec-first-price spec-price-bigger">
-                    ${data.MoneyRaised}
-                  </div>
-                  <div className="col-6 spec-second-price spec-price-bigger">
-                    ${data.TotalValue}
-                  </div>
-                  <div className="col-12 ">
-                    <ProgressBar
-                      progress={percentage(data.MoneyRaised, data.TotalValue)}
-                    />
-                  </div>
-                  <div className="col-12">
-                    <div className="projects-description__stats">
-                      <div className="-primary-color">
-                        <p>{data.PanelSize}</p>
-                        <h6>Solar</h6>
-                      </div>
-                      <div className="-dark-color">
-                        <p>{batteryArray ? batteryArray[batteryArray.length - 1] : ''}</p>
-                        <h6>Storage</h6>
-                      </div>
-                      <div>
-                        <p>{data.ExplorePageSummary.Return}</p>
-                        <h6>Return</h6>
-                      </div>
-                      <div>
-                        <p>{data.Rating}</p>
-                        <h6>Rating</h6>
-                      </div>
-                      <div>
-                        <p>2025</p>
-                        <h6>Maturity</h6>
-                      </div>
+              <div className="row">
+                <div className="col-6 spec-first-price spec-price-bigger">
+                  ${data.MoneyRaised}
+                </div>
+                <div className="col-6 spec-second-price spec-price-bigger">
+                  ${data.TotalValue}
+                </div>
+                <div className="col-12 ">
+                  <ProgressBar
+                    progress={percentage(data.MoneyRaised, data.TotalValue)}
+                  />
+                </div>
+                <div className="col-12">
+                  <div className="projects-description__stats">
+                    <div className="-primary-color">
+                      <p>{data.PanelSize}</p>
+                      <h6>Solar</h6>
+                    </div>
+                    <div className="-dark-color">
+                      <p>{batteryArray ? batteryArray[batteryArray.length - 1] : ''}</p>
+                      <h6>Storage</h6>
+                    </div>
+                    <div>
+                      <p>{data.ExplorePageSummary.Return}</p>
+                      <h6>Return</h6>
+                    </div>
+                    <div>
+                      <p>{data.Rating}</p>
+                      <h6>Rating</h6>
+                    </div>
+                    <div>
+                      <p>2025</p>
+                      <h6>Maturity</h6>
                     </div>
                   </div>
                 </div>
