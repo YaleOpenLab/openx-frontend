7cbafb44d08f9f5233cf9c521c1959deae3f7283
Varunram <vrg2009@ymail.com>
2019-04-29 14:55:11 +0530

    frontend changes for rwanda project

--- src/components/Explore/SingleProject/ProjectContent/Terms/TermsTable/TermsTable.js
+++ src/components/Explore/SingleProject/ProjectContent/Terms/TermsTable/TermsTable.js
@@ -15,9 +15,23 @@ const TermsTable = props => {
       }
     }
 
+    var uniqueArray = [];
+    var dup = false;
+    for (var i = 0 ; i < props.data.length ; i ++ ) {
+      for (var j = 0 ; j < uniqueArray.length ; j ++) {
+        if (props.data[i].Variable == uniqueArray[j].Variable) {
+          // duplicate, delete
+          dup = true;
+        }
+      }
+      if (!dup) {
+        uniqueArray[i] = props.data[i];
+      }
+    }
+
     items = props.data.map(item => {
       return (
-        <tr key={item.Value}>
+        <tr key={item.Variable}>
           <td>
             <div className="text-with-icon">
               <a href="#" title={item.Variable === "Maturity"? "Maturity is the period by which your bonds mature":
@@ -27,7 +41,8 @@ const TermsTable = props => {
                 item.Variable === "PPA Tariff" || item.Variable === "PPA Avg. Tariff" ? "The tariff dicatated by the power purchase agreement agreed upon":
                 item.Variable === "Return (TEY)" || item.Variable === "Return" || item.Variable === "Exp. Return" ? "The return associated with the project":
                 item.Variable === "Ownership Flip"? "The approximate date when the ownership flip is slated to take place assuming constant payments" :
-                "Default tooltip"
+                item.Variable === "Offtaker1" || item.Variable === "Offtaker2" || item.Variable === "Offtaker2" || item.Variable === "Offtaker4" ? "An offtaker is a person who is responsible for the property described. This entity belongs to a bigger family of recipients" :
+                "Default tooltip which should display info about the stuff defined in the terms table"
               }>
                 {item.Variable} <FontAwesomeIcon icon={faInfoCircle} size="xs"/>
             </a>
