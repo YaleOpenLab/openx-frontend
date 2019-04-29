9d664c3c9ea1e620c85a73947e200c4ee39cf409
Varunram <vrg2009@ymail.com>
2019-04-29 17:39:04 +0530

    more changes

---
+++ src/components/Receiver/Pages/Dashboard/rwanda.js
@@ -0,0 +1,28 @@
+export const rwanda = {
+  receiverData:
+    {
+      fullName: 'Martin Wainstein',
+      projectsActive: '2',
+      currentEnergy: '845 kWh',
+      allTimeEnergy: '10,150 MWh',
+      wallet: '$604.25',
+      autoReload: '28 days',
+      notification: 'None',
+      actionsReq: 'Confirm Auto-Pay',
+    },
+    project: {
+      type: 'SOLAR ONLY',
+      title: 'Rwanda Energy Collective - 100kW',
+      loc: 'Rusizi District, Western Province, Rwanda',
+      category: "MUNI BOND",
+      description: 'This community in Rwanda has no access to electricity yet shows a growing local economy. This microgrid project, developed a collaboration with the Rwandan Green Fund (FONERWA), Yale and MIT, aims to serve 250 homes with solar electricity and electric appliances (e.g. lamps, phone chargers, radio, and battery), as well as a school, the town infirmary led by a team of doctors without borders, and the town hall. This is a community-led cooperative with international backing',
+      description: 'Installation of a proof of concept 1kW solar in the admin building of the Pasto public shool. System has internet connected devices that can securely connect to a blockchain smart contract',
+      ownerName: 'Eric Mbeba',
+      donated: '230\'000',
+      total: '230\'000.00',
+      return: '130%',
+      benefit: '30',
+      maturity: '2025',
+      investBy: 'Aug 2018',
+    },
+};
