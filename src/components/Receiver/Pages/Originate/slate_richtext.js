88fc6ee8358c0f83ab217cbb708c34b3858c1357
pavelkrolevets <pavelkrolevets@gmail.com>
2020-07-08 18:44:01 +0600

    material icons for slate 	modified:   package.json 	modified:   src/components/Receiver/Pages/Originate/Originate.js 	modified:   src/components/Receiver/Pages/Originate/slate_richtext.js

--- src/components/Receiver/Pages/Originate/slate_richtext.js
+++ src/components/Receiver/Pages/Originate/slate_richtext.js
@@ -1,169 +1,230 @@
 import React, { useCallback, useMemo, useState } from 'react'
-import isHotkey from 'is-hotkey'
-import { Editable, withReact, useSlate, Slate } from 'slate-react'
-import { Editor, Transforms, createEditor } from 'slate'
-import { withHistory } from 'slate-history'
+import isHotkey from "is-hotkey";
+import { Editable, withReact, Slate, useSlate } from "slate-react";
+import { createEditor, Editor, Transforms } from "slate";
+import { withHistory } from "slate-history";
 
-import { Button, Icon, Toolbar } from './components'
+import Box from "@material-ui/core/Box";
+import FormatBoldIcon from "@material-ui/icons/FormatBold";
+import FormatItalicIcon from "@material-ui/icons/FormatItalic";
+import FormatUnderlinedIcon from "@material-ui/icons/FormatUnderlined";
+import CodeIcon from "@material-ui/icons/Code";
+import LooksOneIcon from "@material-ui/icons/LooksOne";
+import LooksTwoIcon from "@material-ui/icons/LooksTwo";
+import FormatQuoteIcon from "@material-ui/icons/FormatQuote";
+import FormatListNumberedIcon from "@material-ui/icons/FormatListNumbered";
+import FormatListBulletedIcon from "@material-ui/icons/FormatListBulleted";
+import ToggleButton from "@material-ui/lab/ToggleButton";
+import Divider from "@material-ui/core/Divider";
 
 const HOTKEYS = {
-    'mod+b': 'bold',
-    'mod+i': 'italic',
-    'mod+u': 'underline',
-    'mod+`': 'code',
-}
+    "mod+b": "bold",
+    "mod+i": "italic",
+    "mod+u": "underline",
+    "mod+`": "code"
+};
 
-const LIST_TYPES = ['numbered-list', 'bulleted-list']
-
-const RichTextExample = () => {
-    const [value, setValue] = useState(initialValue)
-    const renderElement = useCallback(props => <Element {...props} />, [])
-    const renderLeaf = useCallback(props => <Leaf {...props} />, [])
-    const editor = useMemo(() => withHistory(withReact(createEditor())), [])
+const RichEditor = () => {
+    const [value, setValue] = useState(initialValue);
+    const renderElement = useCallback(props => <Element {...props} />, []);
+    const renderLeaf = useCallback(props => <Leaf {...props} />, []);
+    const editor = useMemo(() => withHistory(withReact(createEditor())), []);
 
     return (
-        <Slate editor={editor} value={value} onChange={value => setValue(value)}>
-            <Toolbar>
-                <MarkButton format="bold" icon="format_bold" />
-                <MarkButton format="italic" icon="format_italic" />
-                <MarkButton format="underline" icon="format_underlined" />
-                <MarkButton format="code" icon="code" />
-                <BlockButton format="heading-one" icon="looks_one" />
-                <BlockButton format="heading-two" icon="looks_two" />
-                <BlockButton format="block-quote" icon="format_quote" />
-                <BlockButton format="numbered-list" icon="format_list_numbered" />
-                <BlockButton format="bulleted-list" icon="format_list_bulleted" />
-            </Toolbar>
-            <Editable
-                renderElement={renderElement}
-                renderLeaf={renderLeaf}
-                placeholder="Enter some rich text…"
-                spellCheck
-                autoFocus
-                onKeyDown={event => {
-                    for (const hotkey in HOTKEYS) {
-                        if (isHotkey(hotkey, event)) {
-                            event.preventDefault()
-                            const mark = HOTKEYS[hotkey]
-                            toggleMark(editor, mark)
-                        }
-                    }
-                }}
-            />
-        </Slate>
-    )
-}
+        <Box p={1} m={2} border={1} borderColor="grey.500" borderRadius={4}>
+            <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+                <Toolbar>
+                    <MarkButton format="bold">
+                        <FormatBoldIcon />
+                    </MarkButton>
+                    <MarkButton format="italic">
+                        <FormatItalicIcon />
+                    </MarkButton>
+                    <MarkButton format="underline">
+                        <FormatUnderlinedIcon />
+                    </MarkButton>
+                    <MarkButton format="code">
+                        <CodeIcon />
+                    </MarkButton>
+                    <BlockButton format="heading-one">
+                        <LooksOneIcon />
+                    </BlockButton>
+                    <BlockButton format="heading-two">
+                        <LooksTwoIcon />
+                    </BlockButton>
+                    <BlockButton format="block-quote">
+                        <FormatQuoteIcon />
+                    </BlockButton>
+                    <BlockButton format="numbered-list">
+                        <FormatListNumberedIcon />
+                    </BlockButton>
+                    <BlockButton format="bulleted-list">
+                        <FormatListBulletedIcon />
+                    </BlockButton>
+                </Toolbar>
+                <Box pl={1}>
+                    <Editable
+                        renderElement={renderElement}
+                        renderLeaf={renderLeaf}
+                        placeholder="Enter some rich text…"
+                        spellCheck
+                        autoFocus
+                        onKeyDown={event => {
+                            for (const hotkey in HOTKEYS) {
+                                if (isHotkey(hotkey, event)) {
+                                    event.preventDefault();
+                                    const mark = HOTKEYS[hotkey];
+                                    toggleMark(editor, mark);
+                                }
+                            }
+                        }}
+                    />
+                </Box>
+            </Slate>
+        </Box>
+    );
+};
 
-const toggleBlock = (editor, format) => {
-    const isActive = isBlockActive(editor, format)
-    const isList = LIST_TYPES.includes(format)
-
-    Transforms.unwrapNodes(editor, {
-        match: n => LIST_TYPES.includes(n.type),
-        split: true,
-    })
-
-    Transforms.setNodes(editor, {
-        type: isActive ? 'paragraph' : isList ? 'list-item' : format,
-    })
-
-    if (!isActive && isList) {
-        const block = { type: format, children: [] }
-        Transforms.wrapNodes(editor, block)
-    }
-}
-
-const toggleMark = (editor, format) => {
-    const isActive = isMarkActive(editor, format)
-
-    if (isActive) {
-        Editor.removeMark(editor, format)
-    } else {
-        Editor.addMark(editor, format, true)
-    }
-}
-
-const isBlockActive = (editor, format) => {
-    const [match] = Editor.nodes(editor, {
-        match: n => n.type === format,
-    })
-
-    return !!match
-}
-
-const isMarkActive = (editor, format) => {
-    const marks = Editor.marks(editor)
-    return marks ? marks[format] === true : false
-}
-
-const Element = ({ attributes, children, element }) => {
+export const Element = ({ attributes, children, element }) => {
     switch (element.type) {
-        case 'block-quote':
-            return <blockquote {...attributes}>{children}</blockquote>
-        case 'bulleted-list':
-            return <ul {...attributes}>{children}</ul>
-        case 'heading-one':
-            return <h1 {...attributes}>{children}</h1>
-        case 'heading-two':
-            return <h2 {...attributes}>{children}</h2>
-        case 'list-item':
-            return <li {...attributes}>{children}</li>
-        case 'numbered-list':
-            return <ol {...attributes}>{children}</ol>
+        case "block-quote":
+            return <blockquote {...attributes}>{children}</blockquote>;
+        case "bulleted-list":
+            return <ul {...attributes}>{children}</ul>;
+        case "heading-one":
+            return <h1 {...attributes}>{children}</h1>;
+        case "heading-two":
+            return <h2 {...attributes}>{children}</h2>;
+        case "list-item":
+            return <li {...attributes}>{children}</li>;
+        case "numbered-list":
+            return <ol {...attributes}>{children}</ol>;
         default:
-            return <p {...attributes}>{children}</p>
+            return <p {...attributes}>{children}</p>;
     }
-}
+};
 
-const Leaf = ({ attributes, children, leaf }) => {
+export const Leaf = ({ attributes, children, leaf }) => {
     if (leaf.bold) {
-        children = <strong>{children}</strong>
+        children = <strong>{children}</strong>;
     }
 
     if (leaf.code) {
-        children = <code>{children}</code>
+        children = <code>{children}</code>;
     }
 
     if (leaf.italic) {
-        children = <em>{children}</em>
+        children = <em>{children}</em>;
     }
 
     if (leaf.underline) {
-        children = <u>{children}</u>
+        children = <u>{children}</u>;
     }
 
-    return <span {...attributes}>{children}</span>
-}
+    return <span {...attributes}>{children}</span>;
+};
 
-const BlockButton = ({ format, icon }) => {
-    const editor = useSlate()
+const BlockButton = ({ format, children }) => {
+    const editor = useSlate();
     return (
-        <Button
-            active={isBlockActive(editor, format)}
-            onMouseDown={event => {
-                event.preventDefault()
-                toggleBlock(editor, format)
-            }}
-        >
-            <Icon>{icon}</Icon>
-        </Button>
-    )
-}
+        <Box ml={1} mt={1}>
+            <ToggleButton
+                value={format}
+                selected={isBlockActive(editor, format)}
+                onMouseDown={event => {
+                    event.preventDefault();
+                    toggleBlock(editor, format);
+                }}
+                style={{ lineHeight: 1 }}
+            >
+                {children}
+            </ToggleButton>
+        </Box>
+    );
+};
 
-const MarkButton = ({ format, icon }) => {
-    const editor = useSlate()
+const MarkButton = ({ format, children }) => {
+    const editor = useSlate();
     return (
-        <Button
-            active={isMarkActive(editor, format)}
-            onMouseDown={event => {
-                event.preventDefault()
-                toggleMark(editor, format)
-            }}
+        <Box ml={1} mt={1}>
+            <ToggleButton
+                value={format}
+                selected={isMarkActive(editor, format)}
+                onMouseDown={event => {
+                    event.preventDefault();
+                    toggleMark(editor, format);
+                }}
+                style={{ lineHeight: 1 }}
+            >
+                {children}
+            </ToggleButton>
+        </Box>
+    );
+};
+
+const Menu = React.forwardRef(({ children, ...props }, ref) => (
+    <>
+        <Box
+            display="flex"
+            direction="row"
+            justify="flex-start"
+            alignItems="center"
+            flexWrap="wrap"
         >
-            <Icon>{icon}</Icon>
-        </Button>
-    )
-}
+            {children}
+        </Box>
+        <Box pt={2}>
+            <Divider variant="middle" />
+        </Box>
+    </>
+));
+
+const Toolbar = React.forwardRef(({ className, ...props }, ref) => (
+    <Menu {...props} ref={ref} />
+));
+
+const LIST_TYPES = ["numbered-list", "bulleted-list"];
+
+const isBlockActive = (editor, format) => {
+    const [match] = Editor.nodes(editor, {
+        match: n => n.type === format
+    });
+    return !!match;
+};
+
+const isMarkActive = (editor, format) => {
+    const marks = Editor.marks(editor);
+    return marks ? marks[format] === true : false;
+};
+
+const toggleBlock = (editor, format) => {
+    const isActive = isBlockActive(editor, format);
+    const isList = LIST_TYPES.includes(format);
+
+    Transforms.unwrapNodes(editor, {
+        match: n => LIST_TYPES.includes(n.type),
+        split: true
+    });
+
+    Transforms.setNodes(editor, {
+        type: isActive ? "paragraph" : isList ? "list-item" : format
+    });
+
+    if (!isActive && isList) {
+        const block = { type: format, children: [] };
+        Transforms.wrapNodes(editor, block);
+    }
+};
+
+const toggleMark = (editor, format) => {
+    const isActive = isMarkActive(editor, format);
+
+    if (isActive) {
+        Editor.removeMark(editor, format);
+    } else {
+        Editor.addMark(editor, format, true);
+    }
+};
 
 const initialValue = [
     {
@@ -202,4 +263,5 @@ const initialValue = [
     },
 ]
 
-export default RichTextExample
+
+export default RichEditor;
